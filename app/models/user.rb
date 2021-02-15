72c4aa0f142eb7cc3cba96ba4384e896af92fd3e
florence <perezflores34000@gmail.com>
2021-02-15 20:57:10 +0100

    modif

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    after_create :welcome_send
 
     has_many :events
     has_many :attendances
@@ -7,4 +8,9 @@ class User < ApplicationRecord
     validates :last_name, presence: true
     validates :description, presence: true, length: { in: 6..200 }
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email valide" }
+
+
+    def welcome_send
+        UserMailer.welcome_email(self).deliver_now
+    end
 end
